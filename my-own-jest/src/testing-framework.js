47d7bc566a929375059f34ed76fb305800d48462
Hewon Jeong <hewon.jng@gmail.com>
2020-02-16 15:10:01 +0900

    add testing framework

---
+++ my-own-jest/src/testing-framework.js
@@ -0,0 +1,19 @@
+const test = async (title, callback) => {
+  try {
+    await callback()
+    console.log(`✓ ${title}`)
+  } catch (error) {
+    console.error(`✕ ${title}`)
+    console.error(error)
+  }
+}
+
+const expect = actual => ({
+  toBe: expected => {
+    if (actual !== expected) {
+      throw new Error(`${actual} is not equal to ${expected}`)
+    }
+  }
+})
+
+module.exports = { test, expect }
